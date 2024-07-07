feac224400baf5486e279ab4508d6cd866a4230b
marcoP100 <130357199+MarcoP100@users.noreply.github.com>
2024-07-07 20:35:28 +0200

    modifiche per esecuzione su wsl

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,31 @@
 # interruzione in caso di errore
 set -e
 
+# Directory del progetto
+PROJECT_DIR=$(dirname "$(realpath "$0")")
+
+# Imposta la directory di destinazione sul Raspberry Pi 3
+DEST_DIR="/home/display"
+
+# Definisci la variabile per l'eseguibile
+EXECUTABLE="DinamometerQt"
+
+# Verifica che le variabili d'ambiente siano impostate
+if [ -z "$DEST_DIR" ] || [ -z "$EXECUTABLE" ] || [ -z "$PROJECT_DIR" ]; then
+  echo "DEST_DIR or EXECUTABLE is not set. Please ensure they are set in the task configuration."
+  exit 1
+fi
+
 # Funzione per terminare il processo attivo
 terminate_process() {
   echo "Terminating the existing process on Raspberry Pi 3..."
-  ssh display@169.254.0.2 'pkill -f DinamometerQt'
+  ssh display@169.254.0.2 'pkill -f "${EXECUTABLE}"'
 }
 
 # Termina il processo attivo, se esiste
 terminate_process  # || true
 
-cd /home/marco/progettiQt/DinamometerQt
+cd "${PROJECT_DIR}"
 
 # Funzione per cambiare i permessi della cartella build e dei suoi contenuti
 change_permissions() {
@@ -41,14 +56,14 @@ docker run --rm -u $USER_ID:$GROUP_ID -v $(pwd)/..:/project -w /project/build qt
 docker run --rm -u $USER_ID:$GROUP_ID -v $(pwd)/..:/project -w /project/build qt5-cross-compiler make
 
 # Copia su Raspberry Pi 3
-scp ./DinamometerQt display@169.254.0.2:/home/display/
+scp "${PROJECT_DIR}/build/${EXECUTABLE}" display@169.254.0.2:${DEST_DIR}
 
 # Esecuzione su Raspberry Pi 3
 #ssh -tt display@169.254.0.2 << 'EOF'
 ssh display@169.254.0.2 << 'EOF'
 export QT_QPA_PLATFORM=eglfs
-export LD_LIBRARY_PATH=/home/display
-cd /home/display
+export LD_LIBRARY_PATH="${DEST_DIR}"
+cd "${DEST_DIR}"
 
 #verifico presenza cartella outputHistory
 if [ ! -d "outputHistory" ]; then
@@ -59,7 +74,7 @@ fi
 if [ -f "output.log" ]; then
     mv output.log outputHistory/output-$(date +'%Y%m%d-%H%M%S').log
 fi
-nohup ./DinamometerQt -platform eglfs > output.log 2>&1 &
+nohup "${EXECUTABLE}" -platform eglfs > output.log 2>&1 &
 
 EOF
 
